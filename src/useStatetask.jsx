434aacf12358118d0a2038c889ec51d1298718c9
hudha8590 <hudhathazhathayil@gmail.com>
2025-09-04 16:31:11 +0530

    anotheusestate

--- src/useStatetask.jsx
+++ src/useStatetask.jsx
@@ -1,11 +1,25 @@
+// import React,{useState} from "react";
+// function ButtonLike(){
+//     const [like,setLike]=useState(0)
+//     return (
+//         <div>
+//         <h1>like:{like}</h1>
+//         <button onClick={()=>setLike(like+1)}>Like</button>
+//         </div>
+//     )
+// }
+// export default ButtonLike
+
+//Another Way:
 import React,{useState} from "react";
-function ButtonLike(){
+function countLike(){
     const [like,setLike]=useState(0)
+    const liked=()=>{setLike(like+1)}
     return (
         <div>
-        <h1>like:{like}</h1>
-        <button onClick={()=>setLike(like+1)}>Like</button>
+            <p>like:{like}</p>
+            <button onClick={liked}>LIKE</button>
         </div>
     )
 }
-export default ButtonLike
+export default countLike
